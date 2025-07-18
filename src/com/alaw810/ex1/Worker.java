5a8e3f65e01936efbc8cc9104bc5c019736306cf
Adrià Lorente <adria810@gmail.com>
2025-07-18 19:10:40 +0200

    feat(ex1): create base Worker class with salary calculation

---
+++ src/com/alaw810/ex1/Worker.java
@@ -0,0 +1,41 @@
+package com.alaw810.ex1;
+
+public class Worker {
+    private String name;
+    private String surname;
+    private double hourlyRate;
+
+    public Worker(String name, String surname, double hourlyRate) {
+        this.name = name;
+        this.surname = surname;
+        this.hourlyRate = hourlyRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public double calculateSalary(int hours) {
+        return hours * hourlyRate;
+    }
+}
