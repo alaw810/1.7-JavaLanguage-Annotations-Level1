f6157b493f4f5d93ddf72ae54faa9d4bf2783dee
Adrià Lorente <adria810@gmail.com>
2025-07-22 19:01:11 +0200

    feat: add MainEx1 to demonstrate @Override with Worker hierarchy

---
+++ src/com/alaw810/ex1/MainEx1.java
@@ -0,0 +1,15 @@
+package com.alaw810.ex1;
+
+public class MainEx1 {
+    public static void main(String[] args) {
+        Worker genericWorker = new Worker("Alex", "Smith", 20);
+        RemoteWorker remoteWorker = new RemoteWorker("Jamie", "Brown", 25);
+        OnsiteWorker onsiteWorker = new OnsiteWorker("Taylor", "Green", 22);
+
+        int workedHours = 160;
+
+        System.out.println("Generic worker salary: " + genericWorker.calculateSalary(workedHours));
+        System.out.println("Remote worker salary: " + remoteWorker.calculateSalary(workedHours));
+        System.out.println("Onsite worker salary: " + onsiteWorker.calculateSalary(workedHours));
+    }
+}
